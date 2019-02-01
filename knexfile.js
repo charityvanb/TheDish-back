4fc9995c85f1e872e0e6cbfc7698b92fd87bb5d4
charityvanb <charityvanb@gmail.com>
2019-01-31 20:16:49 -0700

    added knex file

---
+++ knexfile.js
@@ -0,0 +1,22 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './dev.sqlite3'
+    }
+  },
+
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    }
+  }
+
+};
