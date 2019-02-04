1a8a913a0b15e38d44f627f2caf727fee96e9fb7
charityvanb <charityvanb@gmail.com>
2019-02-04 12:30:23 -0700

    worked on routes

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const express = require("express")
 const app = express()
 const bodyParser = require("body-parser")
 const cors = require("cors")
-const port = process.env.PORT || 3007
+const port = process.env.PORT || 3008
