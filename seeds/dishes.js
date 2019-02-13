e3448a271c0dff661581611b35b49c79ebae75a6
charityvanb <charityvanb@gmail.com>
2019-02-13 10:06:08 -0700

    changed text

--- seeds/dishes.js
+++ seeds/dishes.js
@@ -6,7 +6,7 @@ exports.seed = function(knex, Promise) {
       return knex('dishes').insert([
       {"url": "https://s3.amazonaws.com/dishcvb/flowers.jpg",
       "real": true,
-      "uploader": "CVB"
+      "uploader": "CA"
       },
       {"url": "https://s3.amazonaws.com/dishcvb/goldandwhite.jpg",
       "real": true,
@@ -14,7 +14,7 @@ exports.seed = function(knex, Promise) {
       },
       {"url": "https://s3.amazonaws.com/dishcvb/wildrose.jpg",
       "real": true,
-      "uploader": "CVB"
+      "uploader": "CA"
       },
       {"url": "https://s3.amazonaws.com/dishcvb/766fe2ffParadis+bleu+dinner+plate-min__14027.1528393614.jpg",
       "real": true,
@@ -22,7 +22,7 @@ exports.seed = function(knex, Promise) {
       },
       {"url": "https://s3.https://s3.amazonaws.com/dishcvb/20190208_145344.jpg.com/dishcvb/766fe2ffParadis+bleu+dinner+plate-min__14027.1528393614.jpg",
       "real": true,
-      "uploader": "CVB"
+      "uploader": "CA"
       },
       {"url": "https://s3.https://s3.amazonaws.com/dishcvb/20190208_145344.jpg.com/dishcvb/766fe2ffParadis+bleu+dinner+plate-min__14027.https://s3.amazonaws.com/dishcvb/20190208_144116+(1).jpg.jpg",
       "real": true,
