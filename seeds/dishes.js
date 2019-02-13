a107bf678f3e79d14ada3dd21dbf6d6126fd888a
charityvanb <charityvanb@gmail.com>
2019-02-13 02:58:26 -0700

    added more seed data

--- seeds/dishes.js
+++ seeds/dishes.js
@@ -8,11 +8,23 @@ exports.seed = function(knex, Promise) {
       "real": true,
       "uploader": "CVB"
       },
-      {"url": "hhttps://s3.amazonaws.com/dishcvb/goldandwhite.jpg",
+      {"url": "https://s3.amazonaws.com/dishcvb/goldandwhite.jpg",
       "real": true,
       "uploader": "CVB"
       },
       {"url": "https://s3.amazonaws.com/dishcvb/wildrose.jpg",
+      "real": true,
+      "uploader": "CVB"
+      },
+      {"url": "https://s3.amazonaws.com/dishcvb/766fe2ffParadis+bleu+dinner+plate-min__14027.1528393614.jpg",
+      "real": true,
+      "uploader": "CVB"
+      },
+      {"url": "https://s3.https://s3.amazonaws.com/dishcvb/20190208_145344.jpg.com/dishcvb/766fe2ffParadis+bleu+dinner+plate-min__14027.1528393614.jpg",
+      "real": true,
+      "uploader": "CVB"
+      },
+      {"url": "https://s3.https://s3.amazonaws.com/dishcvb/20190208_145344.jpg.com/dishcvb/766fe2ffParadis+bleu+dinner+plate-min__14027.https://s3.amazonaws.com/dishcvb/20190208_144116+(1).jpg.jpg",
       "real": true,
       "uploader": "CVB"
       }
